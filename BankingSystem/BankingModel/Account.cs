96612ec5b2fecc69f2254d17308ad7ef9a47e1d0
Clase <Clase>
2017-03-08 19:24:37 -0500

    Creacion de proyecto Base

---
+++ BankingSystem/BankingModel/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingModel
+{
+    public class Account
+    {
+        //variables de instancia
+        private string _name;
+        //creacion de los metodos set y get
+        public string getName()
+        {
+            return _name;
+        }
+        public void setName(string name)
+        {
+            //_ como el this pero no es this
+            _name = name;
+        }
+    }
+}
